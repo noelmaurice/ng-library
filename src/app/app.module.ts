0c29a9f4d7ecfc5a7f25bdaf0862b5f3dc1d9b7c
noelmaurice <noelmaurice4@gmail.com>
2019-11-02 22:29:35 +0100

    Code refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { SingleBookComponent } from './book-list/single-book/single-book.component';
-import { BookFormComponent } from './book-list/book-form/book-form.component';
-import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { SingleBookComponent } from './components/book-list/single-book/single-book.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { HeaderComponent } from './components/header/header.component';
 import { AuthService } from './services/auth.service';
 import { BooksService } from './services/books.service';
 import { AuthGuardService } from './services/auth-guard.service';
