5eef318b9ceb9c85c4d99cb4903a5b0a5d715b26
noelmaurice <noelmaurice4@gmail.com>
2019-11-03 11:45:18 +0100

    Authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,32 @@
 import { Component } from '@angular/core';
 import * as firebase from 'firebase';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBIEOKV1CT5WYQ05DBR00n_hn9XYc4xB-M",
+  authDomain: "angular-library-53894.firebaseapp.com",
+  databaseURL: "https://angular-library-53894.firebaseio.com",
+  projectId: "angular-library-53894",
+  storageBucket: "angular-library-53894.appspot.com",
+  messagingSenderId: "717089914812",
+  appId: "1:717089914812:web:d7da0008edebe018cc35b9",
+  measurementId: "G-0VJVJF1H2J"
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
   constructor() {
     // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyBIEOKV1CT5WYQ05DBR00n_hn9XYc4xB-M",
-      authDomain: "angular-library-53894.firebaseapp.com",
-      databaseURL: "https://angular-library-53894.firebaseio.com",
-      projectId: "angular-library-53894",
-      storageBucket: "angular-library-53894.appspot.com",
-      messagingSenderId: "717089914812",
-      appId: "1:717089914812:web:d7da0008edebe018cc35b9",
-      measurementId: "G-0VJVJF1H2J"
-    };
-    firebase.initializeApp(firebaseConfig);
+    
+    // firebase.initializeApp(firebaseConfig);
+    
   }
 }
+
+export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,11 @@ import { AuthGuardService } from './services/auth-guard.service';
 const appRoutes: Routes = [
   { path: 'auth/signup', component: SignupComponent },
   { path: 'auth/signin', component: SigninComponent },
-  { path: 'books', component: BookListComponent },
-  { path: 'books/new', component: BookFormComponent },
-  { path: 'books/view/:id', component: SingleBookComponent },
-  { path: '', component: AppComponent },
-  { path: 'not-found', component: AppComponent },
-  { path: '**', redirectTo: 'not-found' }
+  { path: 'books', canActivate: [AuthGuardService], component: BookListComponent },
+  { path: 'books/new', canActivate: [AuthGuardService], component: BookFormComponent },
+  { path: 'books/view/:id', canActivate: [AuthGuardService], component: SingleBookComponent },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '**', redirectTo: 'books' }
 ];
 
 @NgModule({
